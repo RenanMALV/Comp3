0e787078333c2381f05758be3736854e48e31959
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2020-12-12 22:31:22 +0000

    PilhaInt com alocação dinâmica

--- main.cpp
+++ main.cpp
@@ -2,18 +2,49 @@
 using namespace std;
 
 class PilhaInt{
-  #define MAX_PILHA 64
+  #define DEFAULT_SIZE 10
   public:
-    PilhaInt(){
-      topo = -1;
+    //Construtor padrão
+    PilhaInt(int tamanho = DEFAULT_SIZE) :topo(-1){
+      alocar(tamanho);
+    }
+    //construtor de cópia
+    PilhaInt(PilhaInt& p) :topo(-1){
+      alocar(p.capacidade());
+      for(topo = -1; topo<p.topo;){
+        empilha(p.pilha[topo+1]);
+      };
+    }
+    //destrutor
+    ~PilhaInt(){
+      free(pilha);
+    }
+    //alocação dinâmica da pilha
+    void alocar(int tamanho){
+      pilha = (int *) malloc(tamanho*sizeof(int));
+      this->tamanho = tamanho;
+    }
+    //redimensiona o tamanho da pilha
+    void redimensiona(int n){
+      pilha = (int *) realloc(pilha, n*sizeof(int));
+      tamanho = n;
+    }
+    int capacidade() const{
+      return tamanho;
     }
     void empilha(int valor){
+      if(topo==tamanho-1){
+        redimensiona(tamanho*2);
+      }
       pilha[++topo] = valor;
     }
     int desempilha(){
       return pilha[topo--];
     }
+    //exibe a pilha em uma stream de output
     void print(ostream& o){
+      //pilha não vazia
+      if(topo != -1)
       o << "[ ";
       for(int i = -1; i<topo;){
         i++;
@@ -24,29 +55,42 @@ class PilhaInt{
           o << ", ";
       }
     }
+    //override do operador de atribuição
     const PilhaInt& operator = (const PilhaInt& p){
+      free(pilha);
+      alocar(p.capacidade());
       for(topo = -1; topo<p.topo;){
-        topo++;
-        pilha[topo] = p.pilha[topo]; 
+        empilha(p.pilha[topo+1]);  
       }
       return *this;
     }
+    //override do operador de shift para empilhar
     PilhaInt& operator << (int valor){
       empilha(valor);
       return *this;
     }
   private:
-    int pilha[MAX_PILHA];
+    int *pilha;
     int topo;
+    int tamanho;
 };
 
   int main(){
-    PilhaInt q;
-    q << 5;
-    PilhaInt p;
-    p << 25 << 1;
-    q = p;
+    PilhaInt q(2);
+    q << 5 << 6;
     q.print(cout);
-    cout << endl;
+    q.redimensiona(1);
+    q.print(cout);
+
+
+    PilhaInt p(q);
+    p.desempilha();
+    p.print(cout);
+
+    cout << endl << q.capacidade() << "  -  " << p.capacidade() << endl;
+    p << 25 << 26 << 27;
+    q.print(cout);
+    p.print(cout);
+    cout << endl << q.capacidade() << "  -  " << p.capacidade() << endl;
     return 0;
   }
