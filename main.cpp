6b43ccd9be4c2e124b6d3bdaeb1908a56267cc89
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2020-12-31 02:36:11 +0000

    trabalho completo com especificação de templates

--- main.cpp
+++ main.cpp
@@ -1,111 +1,85 @@
+#include <initializer_list>
 #include <iostream>
+
 using namespace std;
 
-class PilhaInt{
-  #define DEFAULT_SIZE 10
+class AbstractPair {
   public:
-    //Construtor padrão
-    PilhaInt(int tamanho = DEFAULT_SIZE) :topo(-1){
-      alocar(tamanho);
-    }
-    //construtor de cópia
-    PilhaInt(PilhaInt& p) :topo(-1){
-      alocar(p.capacidade());
-      for(topo = -1; topo<p.topo;){
-        empilha(p.pilha[topo+1]);
-      };
-    }
-    //destrutor
-    ~PilhaInt(){
-      free(pilha);
-    }
-    //redimensiona o tamanho da pilha
-    void redimensiona(int n){
-      pilha = (int *) realloc(pilha, n*sizeof(int));
-      tamanho = n;
-      if(topo >= n)
-        topo = n-1;
-    }
-    int capacidade() const{
-      return tamanho;
-    }
-    void empilha(int valor){
-      if(topo==tamanho-1){
-        redimensiona(tamanho*2);
-      }
-      pilha[++topo] = valor;
-    }
-    int desempilha(){
-      return pilha[topo--];
-    }
-    //exibe a pilha em uma stream de output
-    void print(ostream& o){
-      //pilha não vazia
-      if(topo != -1)
-      o << "[ ";
-      for(int i = -1; i<topo;){
-        i++;
-        o << pilha[i];
-        if(i == topo)
-          o << " ]";
-        else
-          o << ", ";
-      }
-    }
-    //override do operador de atribuição
-    const PilhaInt& operator = (const PilhaInt& p){
-      if(this != &p){
-        free(pilha);
-        alocar(p.capacidade());
-        for(topo = -1; topo<p.topo;){
-          empilha(p.pilha[topo+1]);  
-        }
-      }
-      return *this;
-    }
-    //override do operador de shift para empilhar
-    PilhaInt& operator << (int valor){
-      empilha(valor);
-      return *this;
-    }
-  private:
-    int *pilha;
-    int topo;
-    int tamanho;
-
-    //alocação dinâmica da pilha
-    void alocar(int tamanho){
-      pilha = (int *) malloc(tamanho*sizeof(int));
-      this->tamanho = tamanho;
-    }
+    virtual void print(ostream& o) = 0;
 };
 
-  int main(){
-    /*PilhaInt q(2);
-    q << 5 << 6;
-    q.print(cout);
-    q.redimensiona(1);
-    q.print(cout);
+template <typename A, typename B>
+class PairImpl : public AbstractPair{
+  public:
+    PairImpl(A argA, B argB) : a(argA), b(argB){
+    }
+    void print(ostream& o){
+      //cout << endl << "[debug] called generic print inside a pair implementation" << endl;
+      o << a << " = " << b;
+    }
+  private:
+    A a;
+    B b;
+};
 
+template <>
+class PairImpl <const char*, int> : public AbstractPair{
+  public:
+    PairImpl(const char* argA, int argB) : a(argA), b(argB){
+       //cout << endl << "[debug] called pair implementation assign constructor" << endl;
+    }
+    void print(ostream& o){
+       //cout << endl << "[debug] called print inside a pair implementation and send result to output stream" << endl;
+      o << a << " = " << b;
+    }
+  private:
+    const char* a;
+    int b;
+};
 
-    PilhaInt p(q);
-    p.desempilha();
-    p.print(cout);
+template <>
+class PairImpl<int, const char*> : public AbstractPair{
+  public:
+    PairImpl(int argA, const char* argB) : a(argA), b(argB){}
+    void print(ostream& o){
+      o << a << " = " << b;
+    }
+  private:
+    int a;
+    const char* b;
+};
 
-    cout << endl << q.capacidade() << "  -  " << p.capacidade() << endl;
-    p << 25 << 26 << 27;
-    q.print(cout);
-    p.print(cout);
-    cout << endl << q.capacidade() << "  -  " << p.capacidade() << endl;
-
-    q = q;
-    
-    PilhaInt a{7};
-    PilhaInt b{7};
-    a << 8 << 3 << 1 << 4 << 5;
-    b << 0 << 0;
-    a = b;
-    a.print( cout ); cout << endl;
-    */
-    return 0;
+class Pair {
+public:
+  template <typename A, typename B>
+  Pair( A a, B b ) {
+    //especialização do tipo de par
+    p = new PairImpl<A, B>(a ,b );
   }
+  void print(ostream& o){
+    //cout << endl << "[debug] called print inside a pair" << endl;
+    p->print(o);
+  }
+private:
+  AbstractPair *p = nullptr;
+};
+
+void print( ostream& o, initializer_list<Pair> lista ) {
+  //cout << endl << "[debug] static print called" << endl;
+  for( Pair par : lista){
+    //cout << endl << "[debug] Encountered a Pair in initializer list" << endl;
+    par.print(o);
+    o << endl;
+  }
+}
+
+int main() {
+
+  Pair p( "1", 2 );
+
+  //print(cout, {p});
+
+  print( cout, { { "jan", 1 }, { 2, "fev" }, { string( "pi" ), 3.14 } } );
+
+  return 0;  
+}
