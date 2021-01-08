9a23c571d049ba07f9de0347c7d33d9272746688
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2021-01-08 17:52:46 +0000

    trabalho completo e rodando para Array de int, double ... e vector(estruturas com suporte à begin e end).

--- main.cpp
+++ main.cpp
@@ -3,17 +3,28 @@
 
 using namespace std;
 
-template<typename T, >
-T operator | (  ) {
-      ;
-      return ;
-    }
+template<typename T, typename F>
+T* operator | (T& t, F func ) {
+  for(auto x : t){
+    func(x);
+  }
+  return &t;
+}
+template<typename ArrT, int n,typename F>
+ArrT* operator | (ArrT (&arr)[n], F func ) {
+  for(int i=0; i<n; ++i){
+    func(arr[i]);
+  }
+  return arr;
+}
+
+void print( int x ) { cout << x << " "; }
 
 int main( int argc, char* argv[]) {     
 
-  int tab[10] =  { 1, 2, 3, 2, 3, 4, 6, 0, 1, 8 };
+  double tab[10] =  { 1, 2, 3, 2, 3, 4, 6, 0, 1, 8 };
   vector<int> v{ 2 , 6, 8 };
-  tab | []( int x ) { cout << x*x << endl; };
+  tab | []( double x ) { cout << x*x << endl; };
   tab | [ &v ]( int x ) { v.push_back( x ); };
   v | []( int x ) { cout << x*x << endl; };
   v | &print;
