65ab18480845d4fd744c4621294011c5a4ed99e6
Renan Mendanha <renanma@dcc.ufrj.br>
2021-02-05 13:09:58 +0000

    1 commit

--- main.cpp
+++ main.cpp
@@ -1,110 +1,140 @@
 #include <iostream>
+#include <type_traits>
+#include <vector>
+#include <functional>
+
 using namespace std;
 
-class PilhaInt{
-  #define DEFAULT_SIZE 10
+class X {
   public:
-    //Construtor padrão
-    PilhaInt(int tamanho = DEFAULT_SIZE) :topo(-1){
-      alocar(tamanho);
-    }
-    //construtor de cópia
-    PilhaInt(PilhaInt& p) :topo(-1){
-      alocar(p.capacidade());
-      for(topo = -1; topo<p.topo;){
-        empilha(p.pilha[topo+1]);
-      };
-    }
-    //destrutor
-    ~PilhaInt(){
-      free(pilha);
-    }
-    //redimensiona o tamanho da pilha
-    void redimensiona(int n){
-      pilha = (int *) realloc(pilha, n*sizeof(int));
-      tamanho = n;
-      if(topo >= n)
-        topo = n-1;
-    }
-    int capacidade() const{
-      return tamanho;
-    }
-    void empilha(int valor){
-      if(topo==tamanho-1){
-        redimensiona(tamanho*2);
-      }
-      pilha[++topo] = valor;
-    }
-    int desempilha(){
-      return pilha[topo--];
-    }
-    //exibe a pilha em uma stream de output
-    void print(ostream& o){
-      //pilha não vazia
-      if(topo != -1)
-      o << "[ ";
-      for(int i = -1; i<topo;){
-        i++;
-        o << pilha[i];
-        if(i == topo)
-          o << " ]";
-        else
-          o << ", ";
-      }
-    }
-    //override do operador de atribuição
-    const PilhaInt& operator = (const PilhaInt& p){
-      if(this != &p){
-        free(pilha);
-        alocar(p.capacidade());
-        for(topo = -1; topo<p.topo;){
-          empilha(p.pilha[topo+1]);  
-        }
-      }
-      return *this;
-    }
-    //override do operador de shift para empilhar
-    PilhaInt& operator << (int valor){
-      empilha(valor);
-      return *this;
-    }
-  private:
-    int *pilha;
-    int topo;
-    int tamanho;
+  template <typename K>
+  K operator() ( K valor ) {
+    return valor;
+  }
+  auto operator% ( int div ){
+    this->div = div;
+    return *this;
+  }
+  auto operator == (int resto){
+    int div = this->div;
+    return [div , resto](auto val){return (val % div == resto) ? true: false;};
+  }
+  int div=2;
+}; 
 
-    //alocação dinâmica da pilha
-    void alocar(int tamanho){
-      pilha = (int *) malloc(tamanho*sizeof(int));
-      this->tamanho = tamanho;
-    }
+
+template <typename T> 
+class Cte {
+public:
+  Cte( const T& valor ): valor( valor ) {}
+
+  template<typename K>
+  T operator() ( K ignorar ) {
+    return valor;
+  } 
+
+private:
+  T valor;
 };
 
+template <typename Op1, typename Op2> 
+class Soma {
+public:
+  Soma(Op1 o1, Op2 o2): a(o1), b(o2) {}
+  template<typename K>
+  auto operator() ( K valor ) {
+      return a( valor ) + b( valor );
+  }
+private:
+  Op1 a;
+  Op2 b;
+};
+
+template <typename Op1, typename Op2> 
+class Mult {
+public:
+  Mult(Op1 o1, Op2 o2): a(o1), b(o2) {}
+  template<typename K>
+  auto operator() ( K valor ) {
+      return a( valor ) * b( valor );
+  }
+private:
+  Op1 a;
+  Op2 b;
+};
+
+template <typename Op1, typename Op2>
+auto operator + ( Op1 a, Op2 b ) {
+  if constexpr( is_same_v< Op1, int> || is_same_v< Op1,const char*> || is_same_v< Op1, double>)
+    if constexpr( is_same_v< Op2, int> || is_same_v< Op2,const char*> || is_same_v< Op2, double>)
+      return Soma< Cte<Op1>, Cte<Op2> >( Cte<Op1>(a), Cte<Op2>(b) );
+    else
+      return Soma< Cte<Op1>, Op2 >( Cte<Op1>(a), b);
+  else
+    if constexpr( is_same_v< Op2, int> || is_same_v< Op2,const char*> || is_same_v< Op2, double>)
+      return Soma< Op1, Cte<Op2> >( a, Cte<Op2>(b));
+    else
+      return Soma< Op1, Op2 >( a, b);  
+}
+
+template <typename Op1, typename Op2>
+auto operator * ( Op1 a, Op2 b ) {
+  if constexpr( is_same_v< Op1, int> || is_same_v< Op1,const char*> || is_same_v< Op1, double>)
+    if constexpr( is_same_v< Op2, int> || is_same_v< Op2,const char*> || is_same_v< Op2, double>)
+      return Mult< Cte<Op1>, Cte<Op2> >( Cte<Op1>(a), Cte<Op2>(b) );
+    else
+      return Mult< Cte<Op1>, Op2 >( Cte<Op1>(a), b);
+  else
+    if constexpr( is_same_v< Op2, int> || is_same_v< Op2,const char*> || is_same_v< Op2, double>)
+      return Mult< Op1, Cte<Op2> >( a, Cte<Op2>(b));
+    else
+      return Mult< Op1, Op2 >( a, b);  
+}
+
+
+
+template <typename Op1, typename Op2>
+auto operator << ( ostream& o, const Mult<Op1,Op2>& opr) { 
+ return  [&o,opr]( auto v ) -> ostream& { return o << opr(v); };
+}
+
+template <typename V, typename F>
+auto operator | ( const V& v, F func ) {
+    if constexpr( is_same_v< bool, invoke_result_t< F, decltype( *begin( v ) ) > > ) {
+        vector< decay_t< decltype( *begin( v ) ) > > res;
+ 
+	for( auto x: v )
+            if( invoke( func, x ) )
+                res.push_back( x );
+
+        return res;
+    }
+    else if constexpr( is_same_v< void, invoke_result_t< F, decltype( *begin( v ) ) > > ) {
+        for( auto x: v )
+            func( x );
+    }
+    else {
+        vector< decay_t< invoke_result_t< F, decltype( *begin( v ) ) > > > res;
+        
+        for( auto x: v )
+            res.push_back( invoke( func, x ) );
+            
+        return res;
+    }  
+}
+
+X x;
+
   int main(){
-    /*PilhaInt q(2);
-    q << 5 << 6;
-    q.print(cout);
-    q.redimensiona(1);
-    q.print(cout);
-
-
-    PilhaInt p(q);
-    p.desempilha();
-    p.print(cout);
-
-    cout << endl << q.capacidade() << "  -  " << p.capacidade() << endl;
-    p << 25 << 26 << 27;
-    q.print(cout);
-    p.print(cout);
-    cout << endl << q.capacidade() << "  -  " << p.capacidade() << endl;
-
-    q = q;
-    */
-    PilhaInt a{7};
-    PilhaInt b{7};
-    a << 8 << 3 << 1 << 4 << 5;
-    b << 0 << 0;
-    a = b;
-    a.print( cout ); cout << endl;
+    //vector<int> v1 = { 1, 2, 3, 4, 1, 1, 0, 8 };
+    //v1 | (x % 2 == 0) | cout << x+x;
+    //auto s = x + x + 8 + x;
+    //cout << s(8);
+    //int tab[10] =  { 1, 2, 3, 2, 3, 4, 6, 0, 1, 8 };
+    //vector<int> v;
+    //tab | cout << x*x << '\n';
+    //tab | [ &v ]( int x ) { v.push_back( x ); };
+    //v | x % 2 == 0 | cout << x*x + 1 << '\n';
+    //v | x % 2 == 1 | x*x | cout << x << '\n';
     return 0;
   }
